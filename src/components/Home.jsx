d0646478cd3209a57321e9a92c5cea1b97a86a8c
Andrew Sirico <159565301+siricoa1@users.noreply.github.com>
2025-08-25 15:53:31 -0400

    Updated Home.js
    Needed to update home component for Heroku

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -38,8 +38,9 @@ const Home = ({ user }) => {
   }, [user]);
 
   const removeArticle = (data) => {
+     // remember to change back to "http://localhost:5000" for dev
     const emailData = data[0]
-    fetch(`http://localhost:5000/api/remove?aid=${data[1]}&userEmail=${data[0]}`, {
+    fetch(`https://newsapiapp-a86c0a79e477.herokuapp.com//api/remove?aid=${data[1]}&userEmail=${data[0]}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
