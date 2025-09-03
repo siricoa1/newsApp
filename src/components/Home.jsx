75b6b34fd45a3f51640956b9f824fe9dfe7a53ce
Andrew Sirico <159565301+siricoa1@users.noreply.github.com>
2025-09-03 13:06:50 -0400

    Updated Home.jsx
    Updated Home component to display instructions before user searches for articles.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -53,27 +53,41 @@ const Home = ({ user }) => {
       console.error("Error:",error);
     })
   }
-
-  return (
-    <div id='homeContainer'>
-      <h1 id='homeSavedArticles'>{loading ? "Loading..." : ""}</h1>
-      <div  className="container text-center mt-5">
-        <ol  className="list-group mt-3">
-          {favoriteArticles.map((article) => (
-            <div key={article.id} className="homeArticleDiv">
-              <li className="list-group-item d-flex flex-column align-items-center text-center">
-                <h1>{article.title}</h1>
-                <h3>{article.author}</h3>
-                <img src={article.img} className="img-fluid img-thumbnail my-2" alt="Image Unavailable" style={{ maxWidth: '100%', height: 'auto', maxHeight: '400px' }} ></img>
-                <a href={article.url} className="btn btn-primary mt-2">Read More</a>
-                <button className="btn btn-danger mt-2" onClick={()=>removeArticle([user.email, article.id])}>Remove Article</button>
-              </li>
-            </div>
-          ))}
-        </ol>
+  if (favoriteArticles.length === 0){
+    return(
+      <div id="homeContainer">
+        <div className="container text-center mt-5">
+          <h1>Welcome</h1>
+          <br></br>
+          <br></br>
+          <h3>Saved articles will appear here. To save articles go to the search page and look for an article you like, then hit the save button below. The article</h3>
+          <h3>To log out, click on your avatar at the top left of the screen, this will open a menu and allow you to log out.</h3>
+        </div>
       </div>
-    </div>
-  );
+    );
+
+  } else {
+    return (
+      <div id='homeContainer'>
+        <h1 id='homeSavedArticles'>{loading ? "Loading..." : ""}</h1>
+        <div  className="container text-center mt-5">
+          <ol  className="list-group mt-3">
+            {favoriteArticles.map((article) => (
+              <div key={article.id} className="homeArticleDiv">
+                <li className="list-group-item d-flex flex-column align-items-center text-center">
+                  <h1>{article.title}</h1>
+                  <h3>{article.author}</h3>
+                  <img src={article.img} className="img-fluid img-thumbnail my-2" alt="Image Unavailable" style={{ maxWidth: '100%', height: 'auto', maxHeight: '400px' }} ></img>
+                  <a href={article.url} className="btn btn-primary mt-2">Read More</a>
+                  <button className="btn btn-danger mt-2" onClick={()=>removeArticle([user.email, article.id])}>Remove Article</button>
+                </li>
+              </div>
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Home;
